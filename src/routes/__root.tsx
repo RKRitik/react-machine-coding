6b2b83e22a20d63e950813295bacf87f7e2b502f
ritik <khatri.ritik16@gmail.com>
2024-07-05 00:54:44 +0530

    move header part to root router

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,95 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import stylex from "@stylexjs/stylex";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <div {...stylex.props(styles.root)}>
+      <Header />
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </div>
   ),
+});
+
+const links = [
+  {
+    name: "LINKEDIN",
+    url: "https://www.linkedin.com/in/ritik-khatri-673b62115/",
+  },
+  {
+    name: "GITHUB",
+    url: "https://github.com/RKRitik",
+  },
+  {
+    name: "LEETCODE",
+    url: "https://leetcode.com/u/KhatriRitik/",
+  },
+] as const;
+
+function SocialLinks() {
+  return (
+    <ul {...stylex.props(styles.links)}>
+      {links.map((link) => (
+        <a key={link.name} href={link.url} {...stylex.props(styles.a)}>
+          {link.name}
+        </a>
+      ))}
+    </ul>
+  );
+}
+
+function Header() {
+  return (
+    <div {...stylex.props(styles.header)}>
+      <Link to="/">
+        <img width={40} src={"./logo.svg"} alt="header-logo"></img>
+      </Link>
+      <SocialLinks />
+      <Link to="/about" {...stylex.props(styles.a)}>
+        About me
+      </Link>
+    </div>
+  );
+}
+
+const styles = stylex.create({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+    backgroundColor: "#101014",
+    padding: {
+      default: "20px 200px",
+      "@media (max-width: 852px)": "20px",
+    },
+    boxSizing: "border-box",
+    color: "#F4F1D0",
+    letterSpacing: "0.1em",
+  },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    color: "#F4F1D0",
+    paddingBottom: 64,
+  },
+  links: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 20,
+  },
+  a: {
+    color: {
+      default: "#9BA0B3",
+      ":hover": "#F4F1D0",
+    },
+    textDecoration: "underline 0.15em rgba(255, 0, 0, 0)",
+    transition: "text-decoration-color 300ms ease-in-out",
+    ":hover": {
+      textDecorationColor: "#F4F1D0",
+    },
+  },
 });
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,54 +1,15 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import * as stylex from "@stylexjs/stylex";
 import Card from "../components/card";
 
 export const Route = createFileRoute("/")({
   component: App,
 });
-const links = [
-  {
-    name: "LINKEDIN",
-    url: "https://www.linkedin.com/in/ritik-khatri-673b62115/",
-  },
-  {
-    name: "GITHUB",
-    url: "https://github.com/RKRitik",
-  },
-  {
-    name: "LEETCODE",
-    url: "https://leetcode.com/u/KhatriRitik/",
-  },
-] as const;
-
-function SocialLinks() {
-  return (
-    <ul {...stylex.props(styles.links)}>
-      {links.map((link) => (
-        <a key={link.name} href={link.url} {...stylex.props(styles.a)}>
-          {link.name}
-        </a>
-      ))}
-    </ul>
-  );
-}
-
-function Header() {
-  return (
-    <div {...stylex.props(styles.header)}>
-      <img width={40} src={"./logo.svg"} alt="header-logo"></img>
-      <SocialLinks />
-      <Link to="/about" {...stylex.props(styles.a)}>
-        About me
-      </Link>
-    </div>
-  );
-}
 
 function App() {
   //https://lawsofux.com
   return (
-    <div {...stylex.props(styles.root)}>
-      <Header />
+    <div>
       <p {...stylex.props(styles.subHeading)}>
         Collection of Commonly Asked Components/Features in Frontend React
         Coding Interviews
@@ -59,47 +20,10 @@ function App() {
 }
 
 const styles = stylex.create({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    minHeight: "100vh",
-    backgroundColor: "#101014",
-    padding: {
-      default: "20px 200px",
-      "@media (max-width: 852px)": "20px",
-    },
-    boxSizing: "border-box",
-    color: "#F4F1D0",
-    letterSpacing: "0.1em",
-  },
-  header: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    color: "#F4F1D0",
-    paddingBottom: 64,
-  },
-  links: {
-    display: "flex",
-    flexDirection: "row",
-    gap: 20,
-  },
   subHeading: {
     color: "#F4F1D0",
     fontSize: 32,
     fontWeight: 500,
-  },
-  a: {
-    color: {
-      default: "#9BA0B3",
-      ":hover": "#F4F1D0",
-    },
-    textDecoration: "underline 0.15em rgba(255, 0, 0, 0)",
-    transition: "text-decoration-color 300ms ease-in-out",
-    ":hover": {
-      textDecorationColor: "#F4F1D0",
-    },
   },
 });
 
